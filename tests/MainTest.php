4ef5fc67a38f535cae5d6fde52570eb4537ab299
Garrett W <garrettw87@gmail.com>
2016-12-23 20:37:01 +0000

    updated tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,18 +1,18 @@
 <?php
-use pxgamer\devRant;
+use pxgamer\devRant\Connection;
 
 class MainTest extends PHPUnit_Framework_TestCase
 {
 
     public function testCanBeInitialised()
     {
-        $devRant = new devRant();
-        $this->assertInstanceOf(devRant::class, $devRant);
+        $devRant = new Connection;
+        $this->assertInstanceOf(Connection::class, $devRant);
     }
 
     public function testCanGetRants()
     {
-        $devRant = new devRant();
+        $devRant = new Connection;
         $rants = $devRant->getRants();
         $data = json_decode($rants, true);
         $this->assertArrayHasKey('success', $data);
@@ -20,7 +20,7 @@ class MainTest extends PHPUnit_Framework_TestCase
 
     public function testCanGetRantById()
     {
-        $devRant = new devRant();
+        $devRant = new Connection;
         $rantData = $devRant->getRantById(404);
         $data = json_decode($rantData, true);
         $this->assertArrayHasKey('success', $data);
@@ -28,7 +28,7 @@ class MainTest extends PHPUnit_Framework_TestCase
 
     public function testCanGetUserById()
     {
-        $devRant = new devRant();
+        $devRant = new Connection;
         $userData = $devRant->getUserById(404);
         $data = json_decode($userData, true);
         $this->assertArrayHasKey('success', $data);
@@ -36,7 +36,7 @@ class MainTest extends PHPUnit_Framework_TestCase
 
     public function testCanSearchRants()
     {
-        $devRant = new devRant();
+        $devRant = new Connection;
         $searchData = $devRant->searchRants('Linux');
         $data = json_decode($searchData, true);
         $this->assertArrayHasKey('success', $data);
@@ -44,7 +44,7 @@ class MainTest extends PHPUnit_Framework_TestCase
 
     public function testCanGetUsersId()
     {
-        $devRant = new devRant();
+        $devRant = new Connection;
         $userIdData = $devRant->getUsersId('pxgamer');
         $data = json_decode($userIdData, true);
         $this->assertArrayHasKey('success', $data);
