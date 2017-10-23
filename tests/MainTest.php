b4e075fcea08524d3932e76f227368346145ff07
pxgamer <owzie123@gmail.com>
2017-10-23 09:25:06 +0100

    Updated PhpUnit tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,9 +1,11 @@
 <?php
+
+use PHPUnit\Framework\TestCase;
 use pxgamer\devRant\Connection;
+use pxgamer\devRant\Rant;
 
-class MainTest extends PHPUnit_Framework_TestCase
+class MainTest extends TestCase
 {
-
     public function testCanBeInitialised()
     {
         $devRant = new Connection;
@@ -14,20 +16,22 @@ class MainTest extends PHPUnit_Framework_TestCase
     {
         $devRant = new Connection;
         $rants = $devRant->getRants();
-        $this->assertNotFalse($rants);
+        $this->assertInternalType('array', $rants);
+        $this->assertNotEmpty($rants);
     }
 
     public function testCanGetRantById()
     {
         $devRant = new Connection;
-        $rantData = $devRant->getRantById(404);
-        $this->assertNotFalse($rantData);
+        $rantData = $devRant->getRantById(938509);
+        $this->assertInstanceOf(Rant::class, $rantData);
     }
 
     public function testCanGetUserById()
     {
         $devRant = new Connection;
-        $userData = $devRant->getUserById(404);
+        $userData = $devRant->getUserById(653921);
+        $this->assertInternalType('array', $userData);
         $this->assertArrayHasKey('success', $userData);
     }
 
@@ -35,13 +39,15 @@ class MainTest extends PHPUnit_Framework_TestCase
     {
         $devRant = new Connection;
         $searchData = $devRant->getRants('Linux');
-        $this->assertNotFalse($searchData);
+        $this->assertInternalType('array', $searchData);
+        $this->assertNotEmpty($searchData);
     }
 
     public function testCanGetUserId()
     {
         $devRant = new Connection;
         $userIdData = $devRant->getUserId('pxgamer');
+        $this->assertInternalType('array', $userIdData);
         $this->assertArrayHasKey('success', $userIdData);
     }
 
@@ -49,7 +55,7 @@ class MainTest extends PHPUnit_Framework_TestCase
     {
         $devRant = new Connection;
         $collabs = $devRant->collabs();
+        $this->assertInternalType('array', $collabs);
         $this->assertArrayHasKey('success', $collabs);
     }
-
 }
